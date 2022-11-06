8c427e9103a62c3ac466b444eb20ed731a09f939
josecamelodefreitasneto <camelogamerdf@gmail.com>
2022-11-06 20:05:31 -0300

    arquivos motificado

--- main.js
+++ main.js
@@ -1,42 +1,36 @@
 
-function createGame(player1, hour, player2){
-    return 
+function createGame(player1, hour, player2) {
+    return `
+      <li>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+        <strong>${hour}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+      </li>
     `
-    <li>
-                <img src="./assets/icon-${player1}.svg" 
-                alt="Badeira do ${player1}" />
-                <strong>${hour}</strong>
-                <img src="./assets/icon-${player2}.svg" 
-                alt="Bandeira da ${player2}" />
-            </li>
-    
-    `
-}   
-let delay =-0.4;
-function createCard(date, day, games) {
+  }
+  
+  let delay = -0.4;
+  function createCard(date, day, games) {
     delay = delay + 0.4;
-    return 
-    ` 
-    <div class="card" style="animation-deley: ${delay}s">
+    return `
+      <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
-        ${games}            
+          ${games}
         </ul>
-    </div>
+      </div>
     `
-}
-
-document.querySelector("#cards").innerHTML= 
-    createCard("24/11", "quinta", createGame("brazil","16:00",
-    "serbia")) +
+  }
+  
+  document.querySelector("#cards").innerHTML =
+    createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
     createCard(
-        "28/11", 
-        "segunda",
-         createGame("cameroon","13:00", "brazil")+
-         createGame("portugal","16:00","uruguai")
-         ) +
-    createCard("02/12", "sexta",  createGame("brazil","16:00","switzerland"))
-
+      "28/11",
+      "segunda",
+      createGame("switzerland", "13:00", "brazil") +
+        createGame("portugal", "16:00", "uruguai")
+    ) +
+    createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
 
 
 
